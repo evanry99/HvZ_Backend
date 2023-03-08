cde365cfa974bd22052c196933728aba572eeeb4
Håvard Madland <havmadland@gmail.com>
2023-03-08 09:45:28 +0100

    feat: add kill and player domain models

---
+++ HvZ/Model/Domain/PlayerDomain.cs
@@ -0,0 +1,12 @@
+﻿namespace HvZ.Model.Domain
+{
+    public class PlayerDomain
+    {
+        public int Id { get; set; }
+        public string BiteCode { get; set; }
+        public bool IsPatientZeroero { get; set; }
+        public bool IsHuman { get; set; }
+        public int UserId { get; set; }
+        public int GameId { get; set; }
+    }
+}
