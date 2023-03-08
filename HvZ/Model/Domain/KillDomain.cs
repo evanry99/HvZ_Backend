3900f4384d734f2301f85f580856bcb4f1222693
Håvard Madland <havmadland@gmail.com>
2023-03-08 10:31:04 +0100

    feat: add data annotations to kill domain model

--- HvZ/Model/Domain/KillDomain.cs
+++ HvZ/Model/Domain/KillDomain.cs
@@ -1,14 +1,34 @@
-﻿namespace HvZ.Model.Domain
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HvZ.Model.Domain
 {
-    public class Kill
+    public class KillDomain
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public DateTime TimeOfDeath { get; set; }
+        [MaxLength(50)]
         public string? Story { get; set; }
         public float? Lat { get; set; }
         public float? Lng { get; set; }
+        [Required]
         public int GameId { get; set; }
+        [Required]
         public int KillerId { get; set; }
+        [Required]
         public int VictimId { get; set; }
+
+        //
+        [Required]
+        [ForeignKey("GameId")]
+        public int Game { get; set; }
+        [Required]
+        [ForeignKey("KillerId")]
+        public int Killer { get; set; }
+        [Required]
+        [ForeignKey("VictimId")]
+        public int Victim { get; set; }
     }
 }
