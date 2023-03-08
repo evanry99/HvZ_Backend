08a99b3c5f4c9dca6c3a90fbf2835241f2d12a73
Håvard Madland <havmadland@gmail.com>
2023-03-08 12:22:37 +0100

    feat: add kill and player DTOs

---
+++ HvZ/Model/DTO/PlayerDTO/PlayerCreateDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace HvZ.Model.DTO.PlayerDTO
+{
+    public class PlayerCreateDTO
+    {
+        public string BiteCode { get; set; }
+        public bool IsPatientZero { get; set; }
+        public bool IsHuman { get; set; }
+        public int UserId { get; set; }
+        public int GameId { get; set; }
+    }
+}
---
+++ HvZ/Model/DTO/PlayerDTO/PlayerReadDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace HvZ.Model.DTO.PlayerDTO
+{
+    public class PlayerReadDTO
+    {
+        public int Id { get; set; }
+        public string BiteCode { get; set; }
+        public bool IsPatientZero { get; set; }
+        public bool IsHuman { get; set; }
+        public int UserId { get; set; }
+        public int GameId { get; set; }
+    }
+}
