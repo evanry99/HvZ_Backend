edb5dc121bfdadc3bbd72ab6320e622b92731eb5
Håvard Madland <havmadland@gmail.com>
2023-03-08 10:35:51 +0100

    feat: add data annotations to player domain model

--- HvZ/Model/Domain/PlayerDomain.cs
+++ HvZ/Model/Domain/PlayerDomain.cs
@@ -1,12 +1,30 @@
-﻿namespace HvZ.Model.Domain
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HvZ.Model.Domain
 {
     public class PlayerDomain
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
+        [MaxLength(20)]
         public string BiteCode { get; set; }
-        public bool IsPatientZeroero { get; set; }
+        [Required]
+        public bool IsPatientZero { get; set; }
+        [Required]
         public bool IsHuman { get; set; }
-        public int UserId { get; set; }
+        [Required]
+        public int UserID { get; set; }
+        [Required]
         public int GameId { get; set; }
+
+        //
+        [Required]
+        [ForeignKey("UserId")]
+        public int User { get; set; }
+        [Required]
+        [ForeignKey("GameId")]
+        public int Game { get; set; }
     }
 }
