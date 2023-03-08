c98ad65bc62fc6f9206f22beec0f8a146042d87d
An Experis <an.binh.nguyen@no.experis.com>
2023-03-08 13:04:29 +0100

    feat: add GameDomain in Model

--- HvZ/Model/Domain/GameDomain.cs
+++ HvZ/Model/Domain/GameDomain.cs
@@ -1,6 +1,28 @@
-﻿namespace HvZ.Model.Domain
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HvZ.Model.Domain
 {
     public class GameDomain
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(15)]
+        public string GameState { get; set; }
+        [Required]
+        [MaxLength(400)]
+        public string Description { get; set; }
+
+
+        //Coordinates
+        public float? Nw_Lat { get; set; }
+        public float? Nw_Lng { get; set; }
+        public float? Se_Lat { get; set; }
+        public float? Se_Lng { get; set; }
     }
 }
