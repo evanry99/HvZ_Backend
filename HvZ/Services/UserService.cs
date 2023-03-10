2acda3c5272db3c16c9514de1329462a10b937bd
An Experis <an.binh.nguyen@no.experis.com>
2023-03-10 14:43:45 +0100

    feat: add user service

--- HvZ/Services/UserService.cs
+++ HvZ/Services/UserService.cs
@@ -1,6 +1,51 @@
-﻿namespace HvZ.Services
+﻿using HvZ.Data;
+using HvZ.Model.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace HvZ.Services
 {
-    public class UserService
+    public class UserService : IUserService
     {
+
+        private readonly HvZDbContext _context;
+
+        public UserService(HvZDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<UserDomain> AddUserAsync(UserDomain user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task DeleteUserAsync(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<UserDomain>> GetAllUserAsync()
+        {
+            return await _context.Users.Include(u => u.Players).ToListAsync();
+        }
+
+        public async Task<UserDomain> GetUserAsync(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task UpdateUserAsync(UserDomain user)
+        {
+            _context.Entry(user).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public bool UserExists(int id)
+        {
+            return _context.Users.Any(u => u.Id == id);
+        }
     }
 }
