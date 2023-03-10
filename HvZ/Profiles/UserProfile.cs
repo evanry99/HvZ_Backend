555539663c43f6e8f1c0da74ce68a7009e2d410d
An Experis <an.binh.nguyen@no.experis.com>
2023-03-10 10:43:00 +0100

    feat: add mapping from userdomain to user DTOs

--- HvZ/Profiles/UserProfile.cs
+++ HvZ/Profiles/UserProfile.cs
@@ -1,6 +1,15 @@
-﻿namespace HvZ.Profiles
+﻿using AutoMapper;
+using HvZ.Model.Domain;
+using HvZ.Model.DTO.UserDTO;
+
+namespace HvZ.Profiles
 {
-    public class UserProfile
+    public class UserProfile: Profile
     {
+        public UserProfile() {
+            CreateMap<UserCreateDTO, UserDomain>();
+            CreateMap<UserDomain, UserReadDTO>();
+            CreateMap<UserDomain, UserEditDTO>();
+        }
     }
 }
