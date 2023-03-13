f14f97d940cfe7b2dba4550775c3b7eeedc763a3
An Experis <an.binh.nguyen@no.experis.com>
2023-03-13 10:02:05 +0100

    feat: add mapping in game profile

--- HvZ/Profiles/GameProfile.cs
+++ HvZ/Profiles/GameProfile.cs
@@ -1,6 +1,16 @@
-﻿namespace HvZ.Profiles
+﻿using AutoMapper;
+using HvZ.Model.Domain;
+using HvZ.Model.DTO.GameDTO;
+using HvZ.Model.DTO.UserDTO;
+
+namespace HvZ.Profiles
 {
-    public class GameProfile
+    public class GameProfile: Profile
     {
+        public GameProfile() {
+            CreateMap<GameCreateDTO, GameDomain>();
+            CreateMap<GameDomain, GameReadDTO>();
+            CreateMap<GameEditDTO, GameDomain>();
+        }
     }
 }
