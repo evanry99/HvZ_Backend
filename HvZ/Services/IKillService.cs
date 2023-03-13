6a37d651c351373d36478f8c9803097bb1dfda39
Håvard Madland <havmadland@gmail.com>
2023-03-13 12:48:59 +0100

    fix: return type in method in kill service interface

--- HvZ/Services/IKillService.cs
+++ HvZ/Services/IKillService.cs
@@ -6,7 +6,7 @@ namespace HvZ.Services
     {
         public Task<IEnumerable<KillDomain>> GetAllKillsAsync(int gameId);
         public Task<KillDomain> GetKillAsync(int gameId, int killId);
-        public Task<KillService> AddKillAsync(int gameId, KillDomain kill);
+        public Task<KillDomain> AddKillAsync(int gameId, KillDomain kill);
         public Task UpdateKillAsync(int gameId, KillDomain kill);
         public Task DeleteKillAsync(int gameId);
         public bool KillExists(int gameId, int killId);
