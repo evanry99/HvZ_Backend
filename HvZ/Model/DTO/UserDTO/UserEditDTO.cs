90c9abef3eb37e2bce823fc05e1335480cc15cc4
An Experis <an.binh.nguyen@no.experis.com>
2023-03-13 09:30:28 +0100

    feat: add id on usereditdto

--- HvZ/Model/DTO/UserDTO/UserEditDTO.cs
+++ HvZ/Model/DTO/UserDTO/UserEditDTO.cs
@@ -2,7 +2,7 @@
 {
     public class UserEditDTO
     {
-
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public bool IsAdmin { get; set; }
