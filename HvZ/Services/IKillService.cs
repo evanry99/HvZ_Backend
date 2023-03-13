e1b904339ef6ff3788469d058895032c3240adcc
Håvard Madland <havmadland@gmail.com>
2023-03-13 10:41:49 +0100

    chore: rename game and kill service methods

--- HvZ/Services/IKillService.cs
+++ HvZ/Services/IKillService.cs
@@ -5,8 +5,8 @@ namespace HvZ.Services
     public interface IKillService
     {
         public Task<IEnumerable<KillDomain>> GetAllKillsAsync(int gameId);
-        public Task<KillDomain> GetSpecificKillAsync(int gameId, int killId);
-        public Task AddKillAsync(int gameId, KillDomain kill);
+        public Task<KillDomain> GetKillAsync(int gameId, int killId);
+        public Task<KillService> AddKillAsync(int gameId, KillDomain kill);
         public Task UpdateKillAsync(int gameId, KillDomain kill);
         public Task DeleteKillAsync(int gameId);
     }
--- HvZ/Services/IPlayerService.cs
+++ HvZ/Services/IPlayerService.cs
@@ -4,9 +4,9 @@ namespace HvZ.Services
 {
     public interface IPlayerService
     {
-        public Task<IEnumerable<PlayerDomain>> GetAllPlayersInGameAsync(int gameId);
-        public Task<PlayerDomain> GetSpecificPlayerInGameAsync(int gameId, int playerId);
-        public Task AddPlayerAsync(int gameId, PlayerDomain player);
+        public Task<IEnumerable<PlayerDomain>> GetAllPlayersAsync(int gameId);
+        public Task<PlayerDomain> GetPlayerAsync(int gameId, int playerId);
+        public Task<PlayerDomain> AddPlayerAsync(int gameId, PlayerDomain player);
         public Task UpdatePlayer(int gameId, int playerId, PlayerDomain player);
         public Task Deleteplayer(int gameId, int playerId);
     }
