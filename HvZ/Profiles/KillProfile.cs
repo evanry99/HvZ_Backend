2bcba2a82d78f65121fddec87064853b9272b8e8
Håvard Madland <havmadland@gmail.com>
2023-03-13 10:49:05 +0100

    feat: add kill and player profiles

---
+++ HvZ/Profiles/KillProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using HvZ.Model.Domain;
+using HvZ.Model.DTO.KillDTO;
+
+namespace HvZ.Profiles
+{
+    public class KillProfile : Profile
+    {
+        public KillProfile()
+        {
+            CreateMap<KillCreateDTO, KillDomain>();
+            CreateMap<KillDomain, KillReadDTO>();
+            CreateMap<KillEditDTO, KillDomain>();
+        }
+    }
+}
