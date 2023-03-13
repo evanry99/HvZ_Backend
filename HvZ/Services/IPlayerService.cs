711a1b8f190de3adfbff45ad69031f88a2d034dc
Håvard Madland <havmadland@gmail.com>
2023-03-13 08:48:57 +0100

    feat: add service classes for kill and player

---
+++ HvZ/Services/IPlayerService.cs
@@ -0,0 +1,10 @@
+﻿using HvZ.Model.Domain;
+
+namespace HvZ.Services
+{
+    public interface IPlayerService
+    {
+        public Task<IEnumerable<PlayerDomain>> GetAllPlayersInGameAsync(int gameId);
+        public Task<PlayerDomain> GetSpecificPlayerInGameAsync(int gameId, int playerId);
+    }
+}
