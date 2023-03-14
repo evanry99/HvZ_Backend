c5022c65083e5bf94b8782ca6c9705f24fa1c055
An Experis <an.binh.nguyen@no.experis.com>
2023-03-14 12:20:48 +0100

    feat: add squad member domain model

---
+++ HvZ/Model/Domain/SquadMemberDomain.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HvZ.Model.Domain
+{
+    public class SquadMemberDomain
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Rank { get; set; }
+        [Required]
+        public int GameId { get; set; }
+        [Required]
+        public int SquadId { get; set; }
+        [Required]
+        public string PlayerId { get; set; }
+
+        // Relationship
+        [ForeignKey("GameId")]
+        public GameDomain Game { get; set; }
+        [ForeignKey("SquadId")]
+        public SquadDomain Squad { get; set; }
+        [ForeignKey("PlayerId")]
+        public PlayerDomain Player { get; set; }
+    }
+}
