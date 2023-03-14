925f4f2f7b32106fc1a973a336382a2a567c894b
Håvard Madland <havmadland@gmail.com>
2023-03-14 12:46:15 +0100

    feat: add squadcheckin model

--- HvZ/Model/Domain/SquadCheckInDomain.cs
+++ HvZ/Model/Domain/SquadCheckInDomain.cs
@@ -1,6 +1,33 @@
-﻿namespace HvZ.Model.Domain
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HvZ.Model.Domain
 {
     public class SquadCheckInDomain
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public DateTime StartTime { get; set; }
+        [Required]
+        public DateTime EndTime { get; set; }
+        [Required]
+        public float Lat { get; set; }
+        [Required]
+        public float Lng { get; set; }
+        [Required]
+        public int GameId { get; set; }
+        [Required]
+        public int SquadId { get; set; }
+        [Required]
+        public int SquadMemberId { get; set; }
+
+        // Relationships
+        [ForeignKey("GameId")]
+        public GameDomain Game { get; set; }
+        [ForeignKey("SquadId")]
+        public SquadDomain Squad { get; set; }
+        [ForeignKey("SquadMemberId")]
+        public SquadCheckInDomain SquadMember { get; set; }
     }
 }
