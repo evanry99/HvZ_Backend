6bf7ff19352e91adfc4d1587876d65c9f9a8b9f6
Håvard Madland <havmadland@gmail.com>
2023-03-14 12:33:30 +0100

    feat: implement chat model

--- HvZ/Model/Domain/ChatDomain.cs
+++ HvZ/Model/Domain/ChatDomain.cs
@@ -1,6 +1,32 @@
-﻿namespace HvZ.Model.Domain
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HvZ.Model.Domain
 {
     public class ChatDomain
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Message { get; set; }
+        [Required]
+        public bool IsHumanGlobal { get; set; }
+        [Required]
+        public bool IsZombieGlobal { get; set; }
+        [Required]
+        public DateTime ChatTime { get; set; }
+        [Required]
+        public int GameId { get; set; }
+        [Required]
+        public int PlayerId { get; set; }
+        public int? SquadId { get; set; }
+
+        // Relationships
+        [ForeignKey("GameId")]
+        public GameDomain Game { get; set; }
+        [ForeignKey("PlayerId")]
+        public PlayerDomain Player { get; set; }
+        [ForeignKey("SquadId")]
+        public SquadDomain? Squad { get; set; }
     }
 }
