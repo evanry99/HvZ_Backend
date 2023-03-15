0b072f9ed9ee9adde7953f385243d296b88e781a
An Experis <an.binh.nguyen@no.experis.com>
2023-03-15 12:46:36 +0100

    feat: change to nullable for squadmember in playerdomain

--- HvZ/Model/Domain/PlayerDomain.cs
+++ HvZ/Model/Domain/PlayerDomain.cs
@@ -25,7 +25,7 @@ namespace HvZ.Model.Domain
         [ForeignKey("GameId")]
         public GameDomain Game { get; set; }
   
-        public SquadMemberDomain SquadMember { get; set; }
+        public SquadMemberDomain? SquadMember { get; set; }
         public ICollection<KillDomain>? Kills { get; set; }
         public ICollection<ChatDomain>? Chats { get; set; }
     }
