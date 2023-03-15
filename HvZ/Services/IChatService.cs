de3d43c76f5285b83d50218497f84996b9bb1ada
Håvard Madland <havmadland@gmail.com>
2023-03-15 12:38:19 +0100

    feat: add chat and squadcheckin services

---
+++ HvZ/Services/IChatService.cs
@@ -0,0 +1,12 @@
+﻿using HvZ.Model.Domain;
+
+namespace HvZ.Services
+{
+    public interface IChatService
+    {
+        public Task<IEnumerable<ChatDomain>> GetChatsAsync(int gameId);
+        public Task<ChatDomain> AddChatAsync(ChatDomain chat, int gameId);
+        public Task DeleteChatAsync(int chatId);
+        public bool ChatExists(int chatId);
+    }
+}
---
+++ HvZ/Services/ISquadCheckInService.cs
@@ -0,0 +1,10 @@
+﻿using HvZ.Model.Domain;
+
+namespace HvZ.Services
+{
+    public interface ISquadCheckInService
+    {
+        public Task<IEnumerable<SquadCheckInDomain>> GetSquadCheckInsAsync(int gameId, int squadId);
+        public Task<SquadCheckInDomain> AddSquadCheckInAsync(SquadCheckInDomain squadCheckIn, int gameId, int squadId);
+    }
+}
