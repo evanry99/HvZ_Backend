ab49f362a00b3b699730bd44e7f13d2e3fddfaef
An Experis <an.binh.nguyen@no.experis.com>
2023-03-15 14:25:22 +0100

    feat: add profiles for squadmember and squad

---
+++ HvZ/Profiles/SquadProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using HvZ.Model.Domain;
+using HvZ.Model.DTO.SquadDTO;
+
+namespace HvZ.Profiles
+{
+    public class SquadProfile: Profile
+    {
+        public SquadProfile() 
+        {
+            CreateMap<SquadCreateDTO, SquadDomain>();
+            CreateMap<SquadDomain, SquadReadDTO>();
+            CreateMap<SquadEditDTO, SquadDomain>();
+        }
+    }
+}
