0301aeb309e92cb6481684e101c6bdf853de1c09
Håvard Madland <havmadland@gmail.com>
2023-03-15 09:11:45 +0100

    feat: add chat profile

---
+++ HvZ/Profiles/ChatProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using HvZ.Model.Domain;
+using HvZ.Model.DTO.ChatDTO;
+
+namespace HvZ.Profiles
+{
+    public class ChatProfile : Profile
+    {
+        public ChatProfile()
+        {
+            CreateMap<ChatCreateDTO, ChatDomain>();
+            CreateMap<ChatDomain, ChatReadDTO>();
+            CreateMap<ChatEditDTO, ChatDomain>();
+        }
+    }
+}
