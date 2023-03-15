405101b53d9ca91880d4b20643452b448f1657c4
An Experis <an.binh.nguyen@no.experis.com>
2023-03-15 10:05:05 +0100

    feat: add db context for new models in hvzdbcontext.cs

--- HvZ/Data/HvZDbContext.cs
+++ HvZ/Data/HvZDbContext.cs
@@ -13,6 +13,11 @@ namespace HvZ.Data
         public DbSet<KillDomain> Kills { get; set; }
         public DbSet<PlayerDomain> Players { get; set; }
         public DbSet<UserDomain> Users { get; set; }
+        public DbSet<ChatDomain> Chats { get; set; }
+        public DbSet<MissionDomain> Missions { get; set; }
+        public DbSet<SquadCheckInDomain> SquadCheckIns { get; set; }
+        public DbSet<SquadDomain> Squads { get; set; }
+        public DbSet<SquadMemberDomain> SquadMembers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -23,6 +28,17 @@ namespace HvZ.Data
             modelBuilder.Entity<PlayerDomain>().HasData(SeedDataHelper.GetPlayerDomains());
 
             modelBuilder.Entity<UserDomain>().HasData(SeedDataHelper.GetUserDomains());
+
+            modelBuilder.Entity<ChatDomain>().HasData(SeedDataHelper.GetChatDomains());
+
+            modelBuilder.Entity<MissionDomain>().HasData(SeedDataHelper.GetMissionDomains());
+
+            modelBuilder.Entity<SquadDomain>().HasData(SeedDataHelper.GetSquadDomains());
+
+            modelBuilder.Entity<SquadMemberDomain>().HasData(SeedDataHelper.GetSquadMemberDomains());
+
+            modelBuilder.Entity<SquadCheckInDomain>().HasData(SeedDataHelper.GetSquadCheckInDomains());
+
 
 
             //On deleting Kill
@@ -36,6 +52,20 @@ namespace HvZ.Data
                 .HasOne(k => k.Victim)
                 .WithMany()
                 .HasForeignKey(k => k.VictimId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            // On deleting Chats
+            modelBuilder.Entity<ChatDomain>()
+                .HasOne(c => c.Player)
+                .WithMany(p => p.Chats)
+                .HasForeignKey(c => c.PlayerId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            // On deleting SquadMembers
+            modelBuilder.Entity<SquadMemberDomain>()
+                .HasOne(p => p.Squad)
+                .WithMany(s => s.SquadMembers)
+                .HasForeignKey(p => p.PlayerId)
                 .OnDelete(DeleteBehavior.NoAction);
         }
     }
