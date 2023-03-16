11a7cb7fd2157f26f003d109e61b692b9c4dbdb8
An Experis <an.binh.nguyen@no.experis.com>
2023-03-16 12:12:57 +0100

    feat: add interface for squadservice in ISquadService.cs

---
+++ HvZ/Services/ISquadService.cs
@@ -0,0 +1,17 @@
+﻿using HvZ.Model.Domain;
+
+namespace HvZ.Services
+{
+    public interface ISquadService
+    {
+        public Task <IEnumerable<SquadDomain>> GetAllSquadsAsync(int gameId);
+        public Task <SquadDomain> GetSquadAsync(int gameId, int squadId);
+        public Task <SquadDomain> AddSquadAsync(int gameId,SquadDomain squad, int playerId);
+        public Task <SquadMemberDomain> JoinSquadAsync(int gameId, int squadId, SquadMemberDomain squadMember, int playerId);
+        public Task UpdateSquadAsync(int gameId, int squadId, SquadDomain squad);
+        public Task DeleteSquadAsync(int gameId, int squadId);
+        public bool SquadExistsAsync(int squadId);
+
+
+    }
+}
