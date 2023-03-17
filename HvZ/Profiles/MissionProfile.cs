b32e8cb6f458a037d15983aed375b92251eba486
An Experis <an.binh.nguyen@no.experis.com>
2023-03-17 10:00:23 +0100

    feat: add missionprofile

---
+++ HvZ/Profiles/MissionProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using HvZ.Model.Domain;
+using HvZ.Model.DTO.MissionDTO;
+
+namespace HvZ.Profiles
+{
+    public class MissionProfile : Profile
+    {
+        public MissionProfile() 
+        {
+            CreateMap<MissionCreateDTO, MissionDomain>();
+            CreateMap<MissionDomain, MissionReadDTO>();
+            CreateMap<MissionEditDTO, MissionDomain>();
+        }
+    }
+}
