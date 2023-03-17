035426aa89884bbce048db70f6150ac7a051bc4d
Håvard Madland <havmadland@gmail.com>
2023-03-17 15:27:02 +0100

    feat: add mission service to program.cs

--- HvZ/Program.cs
+++ HvZ/Program.cs
@@ -51,6 +51,7 @@ namespace HvZ
             builder.Services.AddScoped(typeof(IKillService), typeof(KillService));
             builder.Services.AddScoped(typeof(IChatService), typeof(ChatService));
             builder.Services.AddScoped(typeof(ISquadCheckInService), typeof(SquadCheckInService));
+            builder.Services.AddScoped(typeof(IMissionService), typeof(MissionService));
 
             var app = builder.Build();
 
