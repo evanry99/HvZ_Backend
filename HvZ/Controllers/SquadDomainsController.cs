c3762a24fcad3df30743b045fd40ad5537d72dbf
Håvard Madland <havmadland@gmail.com>
2023-03-17 10:19:57 +0100

    feat: add squadcheckin post endpoint

--- HvZ/Controllers/SquadDomainsController.cs
+++ HvZ/Controllers/SquadDomainsController.cs
@@ -13,8 +13,12 @@ using AutoMapper;
 
 namespace HvZ.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/squad")]
     [ApiController]
+    [Produces("application/json")]
+    [Consumes("application/json")]
+
+    [ApiConventionType(typeof(DefaultApiConventions))]
     public class SquadDomainsController : ControllerBase
     {
         private readonly HvZDbContext _context;
@@ -32,10 +36,10 @@ namespace HvZ.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<SquadDomain>>> GetSquads()
         {
-          if (_context.Squads == null)
-          {
-              return NotFound();
-          }
+            if (_context.Squads == null)
+            {
+                return NotFound();
+            }
             return await _context.Squads.ToListAsync();
         }
 
@@ -43,10 +47,10 @@ namespace HvZ.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<SquadDomain>> GetSquadDomain(int id)
         {
-          if (_context.Squads == null)
-          {
-              return NotFound();
-          }
+            if (_context.Squads == null)
+            {
+                return NotFound();
+            }
             var squadDomain = await _context.Squads.FindAsync(id);
 
             if (squadDomain == null)
@@ -93,10 +97,10 @@ namespace HvZ.Controllers
         [HttpPost]
         public async Task<ActionResult<SquadDomain>> PostSquadDomain(SquadDomain squadDomain)
         {
-          if (_context.Squads == null)
-          {
-              return Problem("Entity set 'HvZDbContext.Squads'  is null.");
-          }
+            if (_context.Squads == null)
+            {
+                return Problem("Entity set 'HvZDbContext.Squads'  is null.");
+            }
             _context.Squads.Add(squadDomain);
             await _context.SaveChangesAsync();
 
@@ -123,6 +127,12 @@ namespace HvZ.Controllers
             return NoContent();
         }
 
+        /// <summary>
+        /// Get all squad check-ins by gameId and squadId
+        /// </summary>
+        /// <param name="gameId"></param>
+        /// <param name="squadId"></param>
+        /// <returns></returns>
         [HttpGet("{gameId}/squad/{squadId}/check-in")]
         public async Task<ActionResult<IEnumerable<SquadCheckInReadDTO>>> GetSquadCheckIns(int gameId, int squadId)
         {
@@ -133,7 +143,29 @@ namespace HvZ.Controllers
 
             var squadCheckInModel = await _squadCheckInService.GetSquadCheckInsAsync(gameId, squadId);
 
-            return _mapper.Map<List<SquadCheckInReadDTO>>(squadCheckInModel);
+            return Ok(_mapper.Map<List<SquadCheckInReadDTO>>(squadCheckInModel));
+        }
+
+        /// <summary>
+        /// Post a new squad check-in by gameId and squadId
+        /// </summary>
+        /// <param name="squadCheckInDTO"></param>
+        /// <param name="gameId"></param>
+        /// <param name="squadId"></param>
+        /// <returns></returns>
+        [HttpPost("{gameId}/squad/{squadId}/check-in")]
+        public async Task<ActionResult<SquadCheckInReadDTO>> PostSquadCheckIn(SquadCheckInCreateDTO squadCheckInDTO, int gameId, int squadId)
+        {
+            if (!_squadCheckInService.SquadExists(squadId))
+            {
+                return NotFound($"Squad with id {squadId} does not exist");
+            }
+
+            SquadCheckInDomain squadCheckInDomain = _mapper.Map<SquadCheckInDomain>(squadCheckInDTO);
+
+            await _squadCheckInService.AddSquadCheckInAsync(squadCheckInDomain, gameId, squadId);
+
+            return CreatedAtAction("PostSquadCheckIn", new { id = squadCheckInDomain.Id }, squadCheckInDomain);
         }
 
         private bool SquadDomainExists(int id)
