51395cf3fe66b7efcd2403375a579d1d9c183e94
An Experis <an.binh.nguyen@no.experis.com>
2023-03-17 17:45:56 +0100

    feat: add and change values in isquadservice

--- HvZ/Services/ISquadService.cs
+++ HvZ/Services/ISquadService.cs
@@ -7,10 +7,10 @@ namespace HvZ.Services
         public Task <IEnumerable<SquadDomain>> GetAllSquadsAsync(int gameId);
         public Task <SquadDomain> GetSquadAsync(int gameId, int squadId);
         public Task <SquadDomain> AddSquadAsync(int gameId,SquadDomain squad, int playerId);
-        public Task <SquadMemberDomain> AddSquadMemberAsync(int gameId, int squadId, int playerId);
+        public Task <SquadMemberDomain> AddSquadMemberAsync(int gameId, int squadId, SquadMemberDomain squadMember, int playerId);
         public Task <SquadDomain> UpdateSquadAsync(int gameId, int squadId, SquadDomain squad);
         public Task DeleteSquadAsync(int gameId, int squadId);
-        public Task<bool> SquadExistsAsync(int squadId);
+        public Task<bool> SquadExistsAsync(int gameId, int squadId);
         public Task<bool> PlayerAlreadyJoinedGameAsync(int gameId, int playerId);
         public Task<bool> IsHumanAsync(int playerId);
 
