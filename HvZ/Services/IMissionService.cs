ada5c5b6cf46cb30795102248b923b41f979d582
Håvard Madland <havmadland@gmail.com>
2023-03-17 14:59:56 +0100

    feat: add mission service methods deletemission, get all missions & getmission

--- HvZ/Services/IMissionService.cs
+++ HvZ/Services/IMissionService.cs
@@ -1,6 +1,13 @@
-﻿namespace HvZ.Services
+﻿using HvZ.Model.Domain;
+
+namespace HvZ.Services
 {
     public interface IMissionService
     {
+        public Task<IEnumerable<MissionDomain>> GetAllGameMissionsAsync(int gameId);
+        public Task<MissionDomain> GetGameMissionAsync(int gameId, int missionId);
+        public Task<MissionDomain> PostMissionAsync(int gameId);
+        public Task UpdateMissionAsync(MissionDomain mission, int gameId, int missionId);
+        public Task DeleteMissionAsync(int gameId, int missionId);
     }
 }
--- HvZ/Services/MissionService.cs
+++ HvZ/Services/MissionService.cs
@@ -1,7 +1,48 @@
-﻿namespace HvZ.Services
+﻿using HvZ.Data;
+using HvZ.Model.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace HvZ.Services
 {
     public class MissionService : IMissionService
     {
+        public readonly HvZDbContext _context;
+        public MissionService(HvZDbContext context)
+        {
+            _context = context;
+        }
 
+        public async Task DeleteMissionAsync(int gameId, int missionId)
+        {
+            var missionDomain = await _context.Missions.FindAsync(gameId, missionId);
+
+            _context.Missions.Remove(missionDomain);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<MissionDomain>> GetAllGameMissionsAsync(int gameId)
+        {
+            var missions = await _context.Missions.Where(m => m.GameId == gameId)
+                .ToListAsync();
+
+            return missions;
+        }
+
+        public async Task<MissionDomain> GetGameMissionAsync(int gameId, int missionId)
+        {
+            var mission = await _context.Missions.FindAsync(gameId, missionId);
+
+            return mission;
+        }
+
+        public async Task<MissionDomain> PostMissionAsync(int gameId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateMissionAsync(MissionDomain mission, int gameId, int missionId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
