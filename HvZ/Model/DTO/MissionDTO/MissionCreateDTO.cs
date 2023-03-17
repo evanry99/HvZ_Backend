da03ca574ee00976b4c5cb149db10f330de5451c
An Experis <an.binh.nguyen@no.experis.com>
2023-03-17 10:03:15 +0100

    feat: add create and read dto for mission

--- HvZ/Model/DTO/MissionDTO/MissionCreateDTO.cs
+++ HvZ/Model/DTO/MissionDTO/MissionCreateDTO.cs
@@ -2,5 +2,12 @@
 {
     public class MissionCreateDTO
     {
+        public string Name { get; set; }
+        public bool IsHumanVisible { get; set; }
+        public bool IsZombieVisible { get; set; }
+        public string Description { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public int GameId { get; set; }
     }
 }
--- HvZ/Model/DTO/MissionDTO/MissionReadDTO.cs
+++ HvZ/Model/DTO/MissionDTO/MissionReadDTO.cs
@@ -2,5 +2,13 @@
 {
     public class MissionReadDTO
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsHumanVisible { get; set; }
+        public bool IsZombieVisible { get; set; }
+        public string Description { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public int GameId { get; set; }
     }
 }
