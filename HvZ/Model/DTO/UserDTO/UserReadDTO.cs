e44cfa5e858ad3d65ec5b6288ac7f477d0b0de49
An Experis <an.binh.nguyen@no.experis.com>
2023-03-20 10:59:03 +0100

    feat: add username in dto for user

--- HvZ/Model/DTO/UserDTO/UserReadDTO.cs
+++ HvZ/Model/DTO/UserDTO/UserReadDTO.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string UserName { get; set; }
         public bool isAdmin { get; set; }
     }
 }
