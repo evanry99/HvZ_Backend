548c2608eac89d66e9ba1640490b53063ac96c99
An Experis <an.binh.nguyen@no.experis.com>
2023-03-20 12:51:59 +0100

    feat: add migrations

--- HvZ/Migrations/20230320104126_AddUsernameDb.Designer.cs
+++ HvZ/Migrations/20230320104126_AddUsernameDb.Designer.cs
@@ -4,6 +4,7 @@ using HvZ.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HvZ.Migrations
 {
     [DbContext(typeof(HvZDbContext))]
-    partial class HvZDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230320104126_AddUsernameDb")]
+    partial class AddUsernameDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
