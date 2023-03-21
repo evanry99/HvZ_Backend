4ff9d2b9de6735e0078f5073cdb3a12bfac09f3b
An Experis <an.binh.nguyen@no.experis.com>
2023-03-21 13:05:51 +0100

    feat: change in playercreateDTO

--- HvZ/Model/DTO/PlayerDTO/PlayerCreateDTO.cs
+++ HvZ/Model/DTO/PlayerDTO/PlayerCreateDTO.cs
@@ -2,7 +2,7 @@
 {
     public class PlayerCreateDTO
     {
-        public string BiteCode { get; set; }
+        //public string BiteCode { get; set; }
         public bool IsPatientZero { get; set; }
         public bool IsHuman { get; set; }
         public int UserId { get; set; }
