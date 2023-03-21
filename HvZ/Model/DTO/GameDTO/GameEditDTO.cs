15ab1b7f9acb68042345f95d984ac7ecfe5662c4
Håvard Madland <havmadland@gmail.com>
2023-03-21 11:48:36 +0100

    refactor: remove GameId from gameEditDTO

--- HvZ/Model/DTO/GameDTO/GameEditDTO.cs
+++ HvZ/Model/DTO/GameDTO/GameEditDTO.cs
@@ -2,7 +2,6 @@
 {
     public class GameEditDTO
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public string GameState { get; set; }
         public string Description { get; set; }
