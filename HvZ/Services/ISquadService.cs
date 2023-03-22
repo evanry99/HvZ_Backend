74c4f2cdc92454fb932689f6a8ec322f37a34013
An Experis <an.binh.nguyen@no.experis.com>
2023-03-22 10:12:02 +0100

    feat: add playerexists in squadservice and isquadservice

--- HvZ/Services/ISquadService.cs
+++ HvZ/Services/ISquadService.cs
@@ -14,5 +14,7 @@ namespace HvZ.Services
         public Task <bool> PlayerAlreadyJoinedGameAsync(int gameId, int playerId);
         public Task <bool> IsHumanAsync(int playerId);
         public Task <SquadMemberDomain> GetSquadMemberAsync(int gameId, int playerId);
+        public Task<bool> GameExistsAsync(int gameId);
+        public Task<bool> PlayerExistsAsync(int playerId);
     }
 }
