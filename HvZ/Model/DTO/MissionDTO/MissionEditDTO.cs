b502fdf45e5270048507e3a834b23f7d4b9c9dc1
Håvard Madland <havmadland@gmail.com>
2023-03-23 12:22:28 +0100

    feat: add mission lat and lng properties

--- HvZ/Model/DTO/MissionDTO/MissionEditDTO.cs
+++ HvZ/Model/DTO/MissionDTO/MissionEditDTO.cs
@@ -8,6 +8,8 @@
         public string Description { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
+        public double Lat { get; set; }
+        public double Lng { get; set; }
         public int GameId { get; set; }
     }
 }
