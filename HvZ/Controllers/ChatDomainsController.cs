9ebc01b91fea68f1f704db1003e26a00ac31db4f
An Experis <an.binh.nguyen@no.experis.com>
2023-03-24 09:30:20 +0100

    feat: add correct response in chatdomain controller

--- HvZ/Controllers/ChatDomainsController.cs
+++ HvZ/Controllers/ChatDomainsController.cs
@@ -35,9 +35,16 @@ namespace HvZ.Controllers
         /// <param name="gameId"></param>
         /// <returns></returns>
         /// <response code="200"> Success. Return a list of global chats in a game</response>
+        /// <response code="400"> Bad request. </response>
+        /// <response code="401"> Unauthorized </response>
         /// <response code="404"> Game not found. </response>
         /// <response code="500"> Internal error</response>
         [HttpGet("{gameId}/chat/global")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<IEnumerable<ChatReadDTO>>> GetGlobalChats(int gameId)
         {
             if (gameId <= 0)
@@ -62,11 +69,13 @@ namespace HvZ.Controllers
         /// <returns></returns>
         /// <response code="201"> Chat created successfully</response>
         /// <response code="400"> Bad request </response>
-        /// <response code="404"> Bad request </response>
+        /// <response code="401"> Unauthorized </response>
+        /// <response code="404"> Game not found </response>
         /// <response code="500"> Internal error</response>
         [HttpPost("{gameId}/chat")]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
 
@@ -97,9 +106,15 @@ namespace HvZ.Controllers
         /// <returns></returns>
         /// <response code="204"> Chat deleted successfully</response>
         /// <response code="400"> Bad request </response>
+        /// <response code="401"> Unauthorized </response>
         /// <response code="404"> Chat not found</response>
         /// <response code="500"> Internal error</response>
         [HttpDelete("{gameId}/chat/{chatId}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> DeleteChatDomain(int gameId, int chatId)
         {
 
@@ -137,9 +152,15 @@ namespace HvZ.Controllers
         /// <returns></returns>
         /// <response code="200"> Success. Return a list of faction chats in a game</response>
         /// <response code="400"> Bad request. </response>
+        /// <response code="401"> Unauthorized </response>
         /// <response code="404"> Game not found. </response>
         /// <response code="500"> Internal error</response>
         [HttpGet("{gameId}/chat/faction-chat/{playerId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<IEnumerable<ChatReadDTO>>> GetFactionChats(int gameId, int playerId)
         {
             if (gameId <= 0)
@@ -177,9 +198,16 @@ namespace HvZ.Controllers
         /// <param name="gameId"></param>
         /// <returns></returns>
         /// <response code="200"> Success. Return a list of squad chats in a game</response>
+        /// <response code="400"> Bad request. </response>
+        /// <response code="401"> Unauthorized </response>
         /// <response code="404"> Game not found. </response>
         /// <response code="500"> Internal error</response>
         [HttpGet("{gameId}/chat/squad-chat/{squadId}/")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<IEnumerable<ChatReadDTO>>> GetSquadChats(int gameId, int squadId)
         {
             if (gameId <= 0)
@@ -217,9 +245,23 @@ namespace HvZ.Controllers
         /// </summary>
         /// <param name="gameId"></param>
         /// <returns></returns>
+        /// <response code="200"> Success. Return a list of all chats in a game</response>
+        /// <response code="400"> Bad request. </response>
+        /// <response code="401"> Unauthorized </response>
+        /// <response code="404"> Game not found. </response>
+        /// <response code="500"> Internal error</response>
         [HttpGet("{gameId}/chat")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<IEnumerable<ChatReadDTO>>> GetGameChats(int gameId)
         {
+            if (gameId <= 0)
+            {
+                return BadRequest($"Invalid gameId parameter id {gameId}. The gameId must be greater than zero.");
+            }
             if (!_chatService.GameExists(gameId))
             {
                 return NotFound($"Game with id {gameId} does not exist");
